5fc3effc0b1d185635c18ab0c5bdcf56ed52c3b9
KamauDev-maker <oscarnjenga@gmail.com>
2022-10-11 04:02:31 -0700

    frees dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+*free_dog - that frees dogs
+*@d: dog to be freed
+*/
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
