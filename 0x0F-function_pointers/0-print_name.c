7fd8908ab93b4d01c9ae9ed6e33399181648126a
KamauDev-maker <oscarnjenga@gmail.com>
2022-10-13 05:52:27 -0700

    sdr

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,10 @@
  * print_name - Prints a name.
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
- */
+*/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-
-	f(name);
+if (!name || !f)
+return;
+f(name);
 }
