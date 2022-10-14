1bd75d67f9456324e1194696988ad76ec7efa616
KamauDev-maker <oscarnjenga@gmail.com>
2022-10-14 03:30:26 -0700

    corrections

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,32 +7,33 @@
 */
 void print_all(const char * const format, ...)
 {
-va_list ap;
-unsigned int i = 0;
-char *s;
-va_start(ap, format);
-while (format && format[i])
-{
-switch (format[i++])
-{
-case 'c':
-printf("%c", va_arg(ap, char));
-break;
-case 'i':
-printf("%d", va_arg(ap, int));
-break;
-case 'f':
-printf("%f", va_arg(ap, float));
-break;
-case 's':
-s = va_arg(ap, char *);
-printf("%s", s != NULL ? s : "(nil)");
-break;
-default:
-continue;
-}
-if (format[i])
-printf(",");
-}
-printf("\n");
+	va_list arguments;
+	unsigned int i = 0;
+	char *s;
+
+	va_start(arguments, format);
+	while (format && format[i])
+	{
+		switch (format[i++])
+		{
+			case 'c':
+				printf("%c", va_arg(arguments, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(arguments, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(arguments, double));
+				break;
+			case 's':
+				s = va_arg(arguments, char *);
+				printf("%s", s != NULL ? s : "(nil)");
+				break;
+			default:
+				continue;
+		}
+		if (format[i])
+			printf(", ");
+	}
+	printf("\n");
 }
