749565033461ba97fc67366a20fe14089396d463
KamauDev-maker <oscarnjenga@gmail.com>
2022-10-24 03:47:32 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,20 +6,16 @@
 */
 size_t print_list(const list_t *h)
 {
-	register int len = 0;
-
-	while (h)
-	{
-		if (!(h -> str))
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%i] %s\n", h -> len, h -> str);
-		}
-		h = h -> next;
-		len++;
-	}
-	return (len);
+size_t node;
+node = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "(nil)");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+node++;
+}
+return (node);
 }
