f069c0cc21cd51506bb1f2c20b50c6a3e5795592
KamauDev-maker <oscarnjenga@gmail.com>
2022-10-31 04:10:48 -0700

    binary conversion

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,15 +8,17 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int num = 0;
-int len = 0;
-if (b[len] == '\0')
+unsigned int i = 0, num = 0;
+if (b == NULL)
 return (0);
-while ((b[len] == '0') || (b[len] == '1'))
+while (b[i])
 {
+if (!(b[i] == '0' || b[i] == '1'))
+return (0);
 num <<= 1;
-num += b[len] - '0';
-len++;
+if (b[i] == '1')
+num += 1;
+i++;
 }
 return (num);
 }
