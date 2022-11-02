c42f908b6006438eda6997d8427514b6a01b4854
KamauDev-maker <oscarnjenga@gmail.com>
2022-11-02 03:54:32 -0700

    rdwr

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,20 +7,28 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-int file, wr, len = 0;
-if (filename == NULL)
-return (-1);
-file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-if (file == -1)
-return (-1);
-if (text_content != NULL)
-{
-while (text_content[len])
-len++;
-wr = write(file, text_content, len);
-if (wr != len)
-return (-1);
-}
-close(file)
-return (1);
+	int file, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (file == -1)
+		return (-1);
+
+	while (text_content[i])
+		i++;
+
+	if (text_content == NULL)
+	{
+		close(file);
+		return (-1);
+	}
+	else
+	{
+		write(file, text_content, i);
+	}
+
+	close(file);
+	return (1);
 }
