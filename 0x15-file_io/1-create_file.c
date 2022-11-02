b8e4a5ed143b15f8cef03c66d49bcf209528918e
KamauDev-maker <oscarnjenga@gmail.com>
2022-11-02 04:49:29 -0700

    file creation

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,28 +7,21 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i = 0;
+	int fd, chars_written, str_len = 0;
 
 	if (filename == NULL)
 		return (-1);
-
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (file == -1)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
 		return (-1);
-
-	while (text_content[i])
-		i++;
-
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
-		close(file);
-		return (-1);
+		while (text_content[str_len])
+			str_len++;
+		chars_written = write(fd, text_content, str_len);
+		if (chars_written != str_len)
+			return (-1);
 	}
-	else
-	{
-		write(file, text_content, i);
-	}
-
-	close(file);
+	close(fd);
 	return (1);
 }
