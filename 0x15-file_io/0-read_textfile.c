0f92d60ee48c2b1294041243cb2756d0a9cad000
KamauDev-maker <oscarnjenga@gmail.com>
2022-11-02 03:23:32 -0700

    RDONLY

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,20 +11,18 @@ int file, rd, wr;
 char *buf;
 if (filename == NULL)
 return (0);
-file = open(filename, O_RDONLY);
-if (file == -1)
-return (0);
-buf = malloc(sizeof(char) * letters + 1);
+buf = malloc(sizeof(char) * letters);
 if (buf == NULL)
 return (0);
+file = open(filename, O_RDONLY);
 rd = read(file, buf, letters);
-if (rd == -1)
-return (0);
-buf[letters] = '\0';
-wr = write(1, buf, rd);
-if (wr == -1)
-return (0);
-close(file);
+wr = write(STDOUT_FILENO, buf, rd);
+if (file == -1 || rd == -1 || wr == -1 || wr != rd)
+{
 free(buf);
+return (0);
+}
+free(buf);
+close(file);
 return (wr);
 }
